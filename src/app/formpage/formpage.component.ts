39f065c0846ee3aa17883de9d306be4692b449cb
Mack Bakkum <mackbakkum@gmail.com>
2024-10-26 11:05:36 +0200

    init

---
+++ src/app/formpage/formpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './formpage.component.html',
+  styleUrl: './formpage.component.css'
+})
+export class FormpageComponent {
+
+}
