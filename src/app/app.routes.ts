cf3f67e72cd0c4c5d5e1c0ba597de911b4d6f479
Mack Bakkum <mackbakkum@gmail.com>
2024-10-26 11:49:43 +0200

    Q4 main.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FormpageComponent } from './formpage/formpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'formpage', component: FormpageComponent },
+];
