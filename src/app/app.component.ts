989c673479c41016c9403b90f52c2a47ce85d20e
Mack Bakkum <mackbakkum@gmail.com>
2024-10-26 11:35:13 +0200

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormpageComponent } from './formpage/formpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'angbusiness';
 }
--- src/app/formpage/formpage.component.ts
+++ src/app/formpage/formpage.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './formpage.component.html',
   styleUrl: './formpage.component.css'
 })
+
 export class FormpageComponent {
 
 }
