5b0664b973918a7c78c3fd15ceb0c9d7868f54eb
Mack Bakkum <mackbakkum@gmail.com>
2024-10-29 12:21:17 +0100

    initial business overview page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FormpageComponent } from './formpage/formpage.component';
+import {BusinessoverviewComponent} from './businessoverview/businessoverview.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'formpage', component: FormpageComponent },
+  { path: 'businessoverview', component: BusinessoverviewComponent }
 ];
