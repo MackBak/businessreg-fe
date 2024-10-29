240c640fcb9a8d73d0413a4d5263e8ed1c59a8e1
Mack Bakkum <mackbakkum@gmail.com>
2024-10-29 12:46:19 +0100

    basic search page

--- src/app/businessoverview/businessoverview.component.ts
+++ src/app/businessoverview/businessoverview.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-businessoverview',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatIconButton,
+    MatIcon,
+    MatButton
+  ],
   templateUrl: './businessoverview.component.html',
   styleUrl: './businessoverview.component.css'
 })
