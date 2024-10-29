37a401e4e38b1ffad99719709d49eefdc33033f3
Mack Bakkum <mackbakkum@gmail.com>
2024-10-29 11:45:15 +0100

    implement: navbar basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+// app.component.ts
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormpageComponent } from './formpage/formpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormpageComponent],
+  imports: [RouterOutlet, FormpageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
