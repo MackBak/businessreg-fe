41330d571f0e87e2b449cceb2433005e3faaf25b
Mack Bakkum <mackbakkum@gmail.com>
2024-10-29 18:26:54 +0100

    initial form with correct log to console

--- src/app/businessoverview/businessoverview.component.ts
+++ src/app/businessoverview/businessoverview.component.ts
@@ -3,6 +3,8 @@ import {MatFormField} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-businessoverview',
@@ -12,11 +14,29 @@ import {MatIcon} from '@angular/material/icon';
     MatInput,
     MatIconButton,
     MatIcon,
-    MatButton
+    MatButton,
+    ReactiveFormsModule
   ],
   templateUrl: './businessoverview.component.html',
   styleUrl: './businessoverview.component.css'
 })
+
+
 export class BusinessoverviewComponent {
+  searchForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.searchForm = this.fb.group({
+      businessSearch: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit() {
+    if (this.searchForm.valid) {
+      const searchData = this.searchForm.value;
+      console.log(searchData)
+      // TODO: Call Api and get from server.
+    }
+  }
 
 }
