335b2efa709694ded21c24940347e6b1118f9356
Mack Bakkum <mackbakkum@gmail.com>
2024-10-30 09:33:52 +0100

    Http request for search

--- src/app/businessoverview/businessoverview.component.ts
+++ src/app/businessoverview/businessoverview.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatFormField} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-businessoverview',
@@ -27,16 +28,33 @@ export class BusinessoverviewComponent {
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.searchForm = this.fb.group({
-      businessSearch: ['', [Validators.required]]
+      nameOrId: ['', [Validators.required]]
     })
   }
 
   onSubmit() {
     if (this.searchForm.valid) {
-      const searchData = this.searchForm.value;
-      console.log(searchData)
-      // TODO: Call Api and get from server.
+      const inputValue = this.searchForm.get('nameOrId')?.value;
+      const isNumber = !isNaN(inputValue) && inputValue.trim() !== '';
+      const params = new HttpParams().set(isNumber ? 'id' : 'name', inputValue);
+      console.log('These are the params\n: ' + params)
+
+      this.http.get('http://localhost:8080/api/company/get', {params}).subscribe({
+          next: (response) => {
+            console.log('Submitting search', response);
+          },
+          error: (error) => {
+            console.error('Error in form', error);
+          },
+          complete: () => {
+            console.log('Get request completed from api/company')
+          }
+        });
+    } else {
+      console.error('Search not valid');
     }
   }
 
-}
+} // End of export class
+
+
